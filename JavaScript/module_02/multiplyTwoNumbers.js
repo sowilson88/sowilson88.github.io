4bf3d3c353e17ccdfebe3a21aab8fe410c5f0373
sowilson88 <65374755+sowilson88@users.noreply.github.com>
2020-05-21 13:40:48 -0400

    Add files via upload

---
+++ JavaScript/module_02/multiplyTwoNumbers.js
@@ -0,0 +1,4 @@
+var x = 7;
+var y = 16;
+var total = x * y;
+console.log(`${x} * ${y} = ${total}`);
---
+++ JavaScript/module_02/tripleSumTwoNumbers.js
@@ -0,0 +1,5 @@
+var x = 13;
+var y = 6;
+var sumTotal = x + y;
+var total = sumTotal * 3;
+console.log(`${x} + ${y} * 3 = ${total}`);
